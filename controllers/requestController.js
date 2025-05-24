9dce9c4f2c3428e6bd10346740108784a63cb611
Gabriel Rebechi <luiz.rebechi@doc360.com.br>
2025-05-23 21:02:54 -0300

    teste novamente

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -13,9 +13,8 @@ async function createRequest(req, res) {
     if (rows.length > 0) {
       console.log(`Data bruta do banco (rows[0].date_created):`, rows[0].date_created);
       console.log(`Tipo de date_created:`, typeof rows[0].date_created);
-      console.log(`Tipo de date_created:`, rows[0].date_created);
 
-      const last = DateTime.fromJSDate(rows[0].date_created);
+      const last = DateTime.fromJSDate(rows[0].date_created).setZone('America/Sao_Paulo');
       const now = DateTime.now().setZone('America/Sao_Paulo');
 
       console.log(`Interpretação com Luxon - last: ${last.toISO()}`);
